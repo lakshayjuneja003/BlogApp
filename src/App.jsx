f09e179c2250aaff0c777783ca43f6048733ec5c
lakshayjuneja003 <lakshayjuneja003@gmail.com>
2024-01-07 11:00:07 +0530

    .env added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>A Full Stack Blog Application</h1>
+    </>
+  )
+}
+
+export default App
